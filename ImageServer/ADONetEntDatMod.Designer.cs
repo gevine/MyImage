5853045964cfe2aa38257a36e4e15b94d353175b
Levine <levine.esc@gmail.com>
2019-11-12 20:05:35 +0700

    update global file on server & admincontroller

--- ImageServer/ADONetEntDatMod.Designer.cs
+++ ImageServer/ADONetEntDatMod.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Vinh\source\repos\ePrjSem3\git\MyImage\ImageServer\ADONetEntDatMod.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Vinh\source\repos\ePrjSem3\MyImage\ImageServer\ADONetEntDatMod.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
---
+++ ImageServer/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ImageServer.Controllers
+{
+    public class AdminController : ApiController
+    {
+        MyImageEntities db = new MyImageEntities();
+        public IQueryable<Customer> GetCustomers()
+        {
+            return db.Customers;
+        }
+
+        public IQueryable<Role> GetRoles()
+        {
+            return db.Roles;
+        }
+    }
+}
